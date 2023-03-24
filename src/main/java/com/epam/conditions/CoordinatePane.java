657479e547b545bf23a8759f299a6352a72d3b10
MarsX-2002 <mirjalol0331@gmail.com>
2023-03-24 15:29:53 +0500

    done with if-else statement

--- src/main/java/com/epam/conditions/CoordinatePane.java
+++ src/main/java/com/epam/conditions/CoordinatePane.java
@@ -2,7 +2,17 @@ package com.epam.conditions;
 
 public class CoordinatePane {
 
-    public void printQuadrant(int x, int y) {
+    public String printQuadrant(int x, int y) {
+        if (x > 0 && y>0){
+            return "first";
+        } else if (x < 0 && y>0){
+            return "second";
+        } else if (x < 0 && y<0){
+            return "third";
+        } else if (x > 0 && y<0){
+            return "fourth";
+        }
+        return "zero";
     }
 
 }
