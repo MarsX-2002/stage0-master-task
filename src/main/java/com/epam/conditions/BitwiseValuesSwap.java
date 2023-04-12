37f8a93b9b5ea9dcb6eced63794b8fc53705f1b6
MarsX-2002 <mirjalol0331@gmail.com>
2023-04-12 13:47:51 +0500

    master task

--- src/main/java/com/epam/conditions/BitwiseValuesSwap.java
+++ src/main/java/com/epam/conditions/BitwiseValuesSwap.java
@@ -8,7 +8,8 @@ public class BitwiseValuesSwap {
         second = first ^ second;
         first = first ^ second;
 
-        System.out.println(second);
         System.out.println(first);
+        System.out.println(second);
+
     }
 }
